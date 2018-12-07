656dad004fd06a9b994488f4678d4e9bb0af8970
APotapov <potapov.ao@rmsauto.ru>
2018-12-07 14:38:06 +0300

    divide file

---
+++ Models/Block.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gzipper.Models
+{
+    public class Block
+    {
+        public int Size { get; set; }
+        public byte[] Data { get; set; }
+        public byte[] CompressedData { get; set; }
+    }
+}
--- Service.cs
+++ Service.cs
@@ -5,11 +5,14 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace Gzipper
 {
     public class Service 
     {
+        private object locker = new object();
+        private int _size = 1024 * 1024;
         public Service()
         {
 
@@ -21,11 +24,51 @@ namespace Gzipper
             {
                 using (FileStream targetStream = new FileStream(outputFile, FileMode.Create))
                 {
+                   
+                    while (sourceStream.Position < sourceStream.Length) //Пока не прочли весь исходный файл
+                    {
+                        var block = new Block
+                        {
+                            Size = _size,
+                            Data = new byte[_size]
+                        };
+
+                        sourceStream.Read(block.Data, 0, block.Size);
+                        block = DataCompress(block);
+
+                        targetStream.Write(block.CompressedData, 0, block.CompressedData.Length);
+
+                        //Thread _reader = new Thread(() =>
+                        //{
+                        //    block = DataCompress(block);
+                        //    targetStream.Write(block.CompressedData, 0, block.CompressedData.Length);
+                        //});
+                        //_reader.Start();
+
+                        
+                    }
+
                     using (GZipStream compressionStream = new GZipStream(targetStream, CompressionMode.Compress))
                     {
                         sourceStream.CopyTo(compressionStream);
                         Console.WriteLine($"Сжатие файла {inputFile} завершено. Исходный размер: {sourceStream.Length}  сжатый размер: {targetStream.Length}.");
                     }
+                }
+            }
+        }
+
+        private Block DataCompress(Block block)
+        {
+            lock (locker)
+            {
+                using (var data = new MemoryStream())
+                {
+                    using (var compressionStream = new GZipStream(data, CompressionMode.Compress))
+                    {
+                        compressionStream.Write(block.Data, 0, block.Size);
+                        block.CompressedData = data.ToArray();
+                    }
+                    return block;
                 }
             }
         }
